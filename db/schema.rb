1c3c2d88e1f3e1ff317860204cef509c50d8fe85
colby_u <cunderhi@mail.depaul.edu>
2018-03-13 11:57:02 -0500

    Added price columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312021642) do
+ActiveRecord::Schema.define(version: 20180313164711) do
 
   create_table "albums", force: :cascade do |t|
     t.string "albumname"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180312021642) do
     t.integer "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "albumprice"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -95,6 +96,7 @@ ActiveRecord::Schema.define(version: 20180312021642) do
     t.integer "album_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "trackprice"
   end
 
 end
